5edc96db5e38b55a95ff7bc5b9a9ed1e2c74985d
bananagranola <amezster@gmail.com>
2012-09-18 23:00:33 -0700

    added dotfiles update script

---
+++ .scripts/dotfiles_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOTFILES=".dotfiles"
+DOTFILES_DIR="$HOME/$DOTFILES/"
+DOTFILES_DIR_LENGTH=${#DOTFILES_DIR}
+dotfiles=$(find * $DOTFILES_DIR -type f)
+
+for src_dotfile in $dotfiles; do
+	dest_dotfile=${src_dotfile:$DOTFILES_DIR_LENGTH}
+	mkdir -p `dirname $dest_dotfile`
+	if [ ! -h "$HOME/$dest_dotfile" ]; then
+		ln -s "$src_dotfile" "$HOME/$dest_dotfile"
+	fi
+done
+
+find -L $HOME -type l -delete
