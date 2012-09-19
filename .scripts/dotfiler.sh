cacc1d10800dfa43bdfe4fc718777608c03c2795
bananagranola <amezster@gmail.com>
2012-09-19 11:29:15 -0700

    dotfiler update

---
+++ .scripts/dotfiler.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DOTFILES=".dotfiles"
+DOTFILES_DIR="$HOME/$DOTFILES/"
+DOTFILES_DIR_LENGTH=${#DOTFILES_DIR}
+dotfiles=$(find $DOTFILES_DIR -type f)
+
+for src_dotfile in $dotfiles; do
+	dest_dotfile=${src_dotfile:$DOTFILES_DIR_LENGTH}
+	mkdir -p `dirname $dest_dotfile` # exclude .git/ from here
+	if [ ! -h "$HOME/$dest_dotfile" ]; then
+		ln -s "$src_dotfile" "$HOME/$dest_dotfile"
+		echo "$src_dotfile -> $HOME/$dest_dotfile"
+	fi
+done
+
+find -L $HOME -type l -delete
+
+#copy/remove
+#dotfiles=$(git ls-files)
+#dotfiles=$(git ls-files)
+#for dotfile in $dotfiles; do
+#	cp --parents $dotfile /home/amytcheng/.dotfiles
+#	rm $dotfile
+#done
