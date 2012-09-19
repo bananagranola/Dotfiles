3bec87e56d8e990fc9f51b5e3fe38e39f411e395
bananagranola <amezster@gmail.com>
2012-09-18 23:04:24 -0700

    ATCHENG-110

--- .scripts/dotfiles_update.sh
+++ .scripts/dotfiles_update.sh
@@ -3,11 +3,11 @@
 DOTFILES=".dotfiles"
 DOTFILES_DIR="$HOME/$DOTFILES/"
 DOTFILES_DIR_LENGTH=${#DOTFILES_DIR}
-dotfiles=$(find * $DOTFILES_DIR -type f)
+dotfiles=$(find $DOTFILES_DIR -type f)
 
 for src_dotfile in $dotfiles; do
 	dest_dotfile=${src_dotfile:$DOTFILES_DIR_LENGTH}
-	mkdir -p `dirname $dest_dotfile`
+	mkdir -p `dirname $dest_dotfile` # exclude .git/ from here
 	if [ ! -h "$HOME/$dest_dotfile" ]; then
 		ln -s "$src_dotfile" "$HOME/$dest_dotfile"
 	fi
