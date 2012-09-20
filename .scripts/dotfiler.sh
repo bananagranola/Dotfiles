47a5c433b779355d3f5a8082de78d3f5cf979d01
bananagranola <amezster@gmail.com>
2012-09-19 17:58:54 -0700

    ATCHENG-110

--- .scripts/dotfiler.sh
+++ .scripts/dotfiler.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-DOTFILES=".dotfiles"
-DOTFILES_DIR="$HOME/$DOTFILES/"
+DOTFILES_DIR="$HOME/$.dotfiles/"
 DOTFILES_DIR_LENGTH=${#DOTFILES_DIR}
+
+# recurse through $DOTFILES_DIR and get list of dotfiles
 dotfiles=$(find $DOTFILES_DIR -type f)
 
+# iterate through dotfiles in $DOTFILES_DIR
 for src_dotfile in $dotfiles; do
+
+	# strip name of dotfiles folder from source to get destination
+	# example.
+	# 	source: ~/.dotfiles/.mydotfile 
+	# 	destination: ~/.mydotfile
 	dest_dotfile=${src_dotfile:$DOTFILES_DIR_LENGTH}
-	mkdir -p `dirname $dest_dotfile` # exclude .git/ from here
+
+	# create parent folders if necessary
+	mkdir -p `dirname $dest_dotfile`
+
+	# check if symlink to file already exists
 	if [ ! -h "$HOME/$dest_dotfile" ]; then
-		ln -s "$src_dotfile" "$HOME/$dest_dotfile"
-		echo "$src_dotfile -> $HOME/$dest_dotfile"
+		# check if file is in .git
+		if [[ ! $dest_dotfile == .git* ]]; then
+			# symlink the file
+			ln --symbolic --force "$src_dotfile" "$HOME/$dest_dotfile"
+			# notify if link succeeded
+			if [$? -eq 0 ]; then
+				echo "$src_dotfile -> $HOME/$dest_dotfile"
+			else
+				echo "DOTFILER ERROR: $src_dotfile -!> $HOME/$dest_dotfile"
+			fi
+		fi
 	fi
 done
 
+# cleanup dangling symlinks
 find -L $HOME -type l -delete
 
-#copy/remove
-#dotfiles=$(git ls-files)
-#dotfiles=$(git ls-files)
-#for dotfile in $dotfiles; do
-#	cp --parents $dotfile /home/amytcheng/.dotfiles
-#	rm $dotfile
-#done
