c9c8605abd5c89fb716ed6fd3228c5eca6bc21a5
bananagranola <amezster@gmail.com>
2012-10-01 11:22:48 -0700

    modified backup script, added irssi to dmenu, README

--- .scripts/backup.sh
+++ .scripts/backup.sh
@@ -7,17 +7,21 @@ HOME="/home/amytcheng"
 # back up Music folder from Windows partition to external hard drive
 rsync --archive --compress --delete --human-readable --progress --verbose "$WINDOWS/Music/" "$EXTERNAL/Music/"
 
+# move Music folder from external hard drive to Windows partition
+#rsync --archive --compress --delete --human-readable --progress --verbose "$EXTERNAL/Music/" "$WINDOWS/Music/"
+
 # back up Images folder from Windows partition to external hard drive
 #rsync --archive --compress --delete --human-readable --progress --verbose "$WINDOWS/Pictures/" "$EXTERNAL/Images/"
 
 # back up latest Dotfiles git from home to external hard drive
 #git archive -o "$EXTERNAL/home/Dotfiles.tar.gz" HEAD
 
-# back up Applications folder from Dropbox to external hard drive
-#rsync --archive --compress --delete --human-readable --progress --verbose "$HOME/Dropbox/Applications/" "$EXTERNAL/Applications/"
-
 # back up entire hard drive image to external hard drive
 #dd if=/dev/sda conv=sync,noerror bs=64K | gzip -c > "$EXTERNAL/$HOSTNAME.gz"
+
+# monitor dd backup
 #pgrep -l '^dd$'
 #watch -n 10 kill -USR1 $ddpid
+
+# restore dd backup
 #gunzip -c /"$EXTERNAL/$HOSTNAME.gz" | dd of=/dev/hda conv=sync,noerror bs=64K
