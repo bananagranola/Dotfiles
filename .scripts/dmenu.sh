49dcbcd3e2c6df4186d7fb2e13d9174e0a6717e0
bananagranola <amezster@gmail.com>
2013-02-06 11:29:04 -0800

    minor changes

--- .scripts/dmenu.sh
+++ .scripts/dmenu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 FONT="-*-termsyn-medium-*-*-*-11-*-*-*-*-*-*-1"
-NB="#1F1F1F"
-NF="#78956F"
-SB="#3F3F3F"
-SF="#6A8D9B"
+NB="#002b36"
+NF="#268bd2"
+SB="#93a1a1"
+SF="#859900"
 
 /home/amytcheng/.scripts/dmenu_term.sh -b -f -i -fn $FONT -nb $NB -nf $NF -sb $SB -sf $SF &
 exit 0
