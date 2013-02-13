949ac692dee341e279242d860d0c8053a91e7adb
bananagranola <amezster@gmail.com>
2013-02-13 14:40:51 -0800

    echinus: mwcfact .55->.60; dmenu: color swap

--- .scripts/dmenu.sh
+++ .scripts/dmenu.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 FONT="-*-termsyn-medium-*-*-*-11-*-*-*-*-*-*-1"
 NB="#002b36"
-NF="#268bd2"
+NF="#859900"
 SB="#93a1a1"
-SF="#859900"
-
-ARGS="-b -f -i -fn $FONT -nb $NB -nf $NF -sb $SB -sf $SF"
+SF="#268bd2"
 
 cachedir=${XDG_CACHE_HOME:-"$HOME/.cache"}
 if [ -d "$cachedir" ]; then
@@ -16,10 +14,9 @@ fi
 APP=$(
 	IFS=:
 	if stest -dqr -n "$cache" $PATH; then
-		stest -flx $PATH | sort -u | tee "$cache" | dmenu -b -f -i -fn $FONT -b $NB -nf $NF -sb $SB -sf $SF
+		stest -flx $PATH | sort -u | tee "$cache" | dmenu -b -f -i -fn $FONT -nb $NB -nf $NF -sb $SB -sf $SF
 	else
 		dmenu -b -f -i -fn $FONT -nb $NB -nf $NF -sb $SB -sf $SF < "$cache"
 	fi
 )
-
 grep -q -w "$APP" ~/.scripts/dmenu_term.txt && urxvtc -e $APP || echo $APP | ${SHELL:-"/bin/sh"} &
