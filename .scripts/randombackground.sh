bc785d7e57ee99bf3d8e0d0f5118f30f8d1c3306
bananagranola <amezster@gmail.com>
2013-02-27 08:36:29 -0800

    miscellaneous minor changes

--- .scripts/randombackground.sh
+++ .scripts/randombackground.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /home/amytcheng/Images/Wallpapers/ -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z | xargs -0 hsetroot -fill 
+find /home/amytcheng/Images/Wallpapers/ -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z | xargs -0 hsetroot -center
